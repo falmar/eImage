eb46a91f40dc54d17dc856bdaf966feb2e6ab45e
falmar <daviddlavier@gmail.com>
2016-01-04 18:05:37 -0430

    Fix: autoload go back to using a class instead of closure for PHP 5.3 compatibility

--- autoload.php
+++ autoload.php
@@ -11,14 +11,27 @@
  * @license   http://opensource.org/licenses/MIT The MIT License (MIT)
  */
 
-spl_autoload_register(function ($Class) {
-    if (strpos($Class, 'eImage') === 0) {
-        $DS   = DIRECTORY_SEPARATOR;
-        $File = __DIR__ . $DS . str_replace('\\', $DS, $Class) . '.php';
-        if (file_exists($File)) {
-            require_once($File);
+/**
+ * Class eImageAutoload
+ *
+ * @author David Lavieri (falmar) <daviddlavier@gmail.com>
+ */
+class eImageAutoload
+{
+    private static $Prefix = 'eImage';
+
+    static public function load($Class)
+    {
+        if (strpos($Class, self::$Prefix) === 0) {
+            $DS   = DIRECTORY_SEPARATOR;
+            $File = __DIR__ . $DS . str_replace('\\', $DS, $Class) . '.php';
+            if (file_exists($File)) {
+                require_once($File);
+            }
+        } else {
+            return;
         }
-    } else {
-        return;
     }
-});
+}
+
+spl_autoload_register('eImageAutoload::load');
